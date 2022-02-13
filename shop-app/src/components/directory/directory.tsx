81d39a15b29c5cda544d9a4e3c2cdb3822f6ca75
arefworks <aref.works@gmail.com>
2022-02-13 17:49:52 +0100

    support dynamic catergory img

--- shop-app/src/components/directory/directory.tsx
+++ shop-app/src/components/directory/directory.tsx
@@ -3,9 +3,9 @@ import './directory.scss'
 
 export default function Directory () {
   const data = [
-    { id: 1, name: 'Hats' },
-    { id: 2, name: 'Jackets' },
-    { id: 3, name: 'Shoes' }
+    { id: 1, name: 'Hats', imgUrl: 'https://i.ibb.co/cvpntL1/hats.png' },
+    { id: 2, name: 'Jackets', imgUrl: 'https://i.ibb.co/px2tCc3/jackets.png' },
+    { id: 3, name: 'Shoes', imgUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png' }
   ]
 
   return (
--- shop-app/src/components/menu-item/menu-item.tsx
+++ shop-app/src/components/menu-item/menu-item.tsx
@@ -3,7 +3,10 @@ import './menu-item.scss'
 export default function MenuItem ({ item }) {
   return (
     <div className='menu-item'>
-      <div className='background-image'></div>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${item.imgUrl})` }}
+      ></div>
       <div className='title'>{item.name}</div>
       <button className='shop-btm'>shop now</button>
     </div>
