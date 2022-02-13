d4b6448189d39318a86abff9f4a8c319fe2eeae1
arefworks <aref.works@gmail.com>
2022-02-13 16:31:17 +0100

    support typescript

--- shop-app/src/App.js
+++
@@ -1,7 +0,0 @@
-import './App.scss';
-
-function App() {
-  return <div className="App"> hi from the shop app</div>;
-}
-
-export default App;
---
+++ shop-app/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.scss'
+
+function App () {
+  return <div className='App'> hi from the shop app</div>
+}
+
+export default App
